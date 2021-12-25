b7fc8072c68000dff5bf48782327b94c91adb4d3
lordofwizard <advaitpandharpurkar619@gmail.com>
2021-12-25 17:34:21 +0530

    looks like EOF is -1

--- main.c
+++ main.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 int main(){
-
-/*	int fahr, celsius;
-	int lower, upper, step;
-	lower = 0;
-	upper = 300;
-	step = 20;
-	fahr = lower;
-	while(fahr <= upper){
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%3d\t %6d\n", fahr, celsius);
-		fahr = fahr + step;
-	}
-	return 0;*/
-	float fahr;
-	for (fahr = 300; fahr >= 0 ; fahr -=20){
-		printf("%3d \t %4.2f \n",(int)fahr,(5.0/9.0)*(fahr - 32));
-	}
+	printf("%d", EOF);
 	return 0;
+	
 }
