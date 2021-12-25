111c30d440f4992fef80f4dd6d3f3fe7e323e007
lordofwizard <advaitpandharpurkar619@gmail.com>
2021-12-25 18:33:45 +0530

    tried creating a print world by word program

--- main.c
+++ main.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-#define IN 1
-#define OUT 0
 int main(){
-	int c, nl, nw, nc ,state;
-	state = OUT;
-	nl = nw = nc = 0;
-	while((c = getchar()) != EOF){
-		++nc;
-		if (c == '\n') ++nl;
-		if (c == ' ' || c == '\n' || c == '\t')
-			   state = OUT;
-		else if(state == OUT){
-			state = IN;
-			++nw;
-		}
+	int word;
+	int c;
+	while((c = getchar() != EOF)){
+		if(c == ' ' || c == '\t' || c == '\n')
+			   printf("\n");
+		printf("%c",c);
 	}
-	printf ("Lines = %d, Words = %d, Chars = %d\n",nl, nw , nc);
-
-	return 0;
 }
 
